1fecc3cb1f6c1d54af0a4337b838063d337bf415
Vinicius Lisboa <viniciuslisboa1001@gmail.com>
2021-04-05 16:28:39 -0300

    lkjlkj

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -19,38 +19,51 @@ exports.loginAction = async (req, res) => {
         return;
     }
 
-    const data = matchedData(req);
-    const user = await User.findOne({ name: data.name });
-    const payload = (Date.now() + Math.random()).toString();
-    const token = await bcrypt.hash(payload, 10);
-
     try {
+        const data = matchedData(req);
+        const user = await User.findOne({ name: data.name });
+        
+        const payload = (Date.now() + Math.random()).toString();
+        const token = await bcrypt.hash(payload, 10);
+
         user.token = token;
         user.save();
-
         req.session.user = user;
         req.session.token = token;
 
         if (user.name == 'recepcao') { 
+
             req.flash('success', 'Login efetuado com sucesso!');
             res.redirect('/');
+
         } else if (user.name == 'eletro') {
+
             req.flash('success', 'Login efetuado na eletro com sucesso!');
-            res.redirect('/eletro')
+            res.redirect('/eletro');
+
         } else if (user.name == 'medicacao') {
+
             req.flash('success', 'Login efetuado em Medicação com sucesso!');
-            res.redirect('/medicacao')
+            res.redirect('/medicacao');
+ 
         } else if (user.name == 'triagem') {
+
             req.flash('success', 'Login efetuado em Triagem com sucesso!');
-            res.redirect('/triagem');
+            res.redirect('/consultorio1');
+
         } else if (user.name == 'consultorio1') {
-            req.flash('success', 'Login efetuado em Consultorio 1 com sucesso!');
-            res.redirect('/consultorio1')
+
+            req.flash('success', 'Login efetuado no Consultorio 1 com sucesso');
+            res.redirect('/consultorio1');
+            
         }
+
  
     } catch (err) {
+        console.log("Erro: " + err);
         req.flash('error', 'Problemas no login!');
         res.redirect("/login");
+
     }
 
 }
